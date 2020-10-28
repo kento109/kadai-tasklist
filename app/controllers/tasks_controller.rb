82a128e921cb9cd9391f54ed7db7393de91b0a73
ketno.sakurai <s.k.sky123@gmail.com>
2020-10-28 12:32:32 +0000

    fix flash_message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,11 +47,10 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
-end
 
+  private
 
-private
-
-def task_params
-  params.require(:task).permit(:content)
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
