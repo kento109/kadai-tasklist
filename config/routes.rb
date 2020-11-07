fa35fb33aad3614cdbf3943aee358768a540cd2e
ketno.sakurai <s.k.sky123@gmail.com>
2020-11-07 05:37:29 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index' 
-
-  resources :tasks
+  
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
