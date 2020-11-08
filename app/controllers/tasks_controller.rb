79735d04dc3eeecce1959c513ad4161b8b1e160d
ketno.sakurai <s.k.sky123@gmail.com>
2020-11-08 04:24:07 +0000

    change redirect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+  
   
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
   end
   
   def show
@@ -14,11 +16,11 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskを作成しました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "Taskを作成できませんでした"
       render :new
@@ -32,7 +34,7 @@ class TasksController < ApplicationController
 
     if @task.update(task_params)
       flash[:success] = 'Taskを更新しました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Taskを更新できませんでした'
       render :edit
@@ -56,4 +58,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
