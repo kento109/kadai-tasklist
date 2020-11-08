df7417343675ef40890210374f8945ab90fbd3af
ketno.sakurai <s.k.sky123@gmail.com>
2020-11-08 09:31:55 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
 
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
